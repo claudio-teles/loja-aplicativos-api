3821b104470cf0ad2066b7da9763aa6c1f25a092
claudio-teles <claudio_teles@yahoo.com>
2021-10-22 08:52:24 -0300

    Commit um

---
+++ src/main/java/com/lojaaplicativosapi/LojaAplicativosApiApplication.java
@@ -0,0 +1,13 @@
+package com.lojaaplicativosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaAplicativosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaAplicativosApiApplication.class, args);
+	}
+
+}
